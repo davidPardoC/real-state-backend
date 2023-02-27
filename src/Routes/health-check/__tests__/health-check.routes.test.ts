057821fdce58a6a7759572deac3db9a4fb46944e
David Pardo <pardodavid10@gmail.com>
2023-02-26 21:20:42 -0500

    Add test for health check

---
+++ src/Routes/health-check/__tests__/health-check.routes.test.ts
@@ -0,0 +1,10 @@
+import supertest from "supertest"
+import { app } from "../../../app"
+
+const api = supertest(app)
+
+describe('HealthCheck', () => { 
+    it("Should get status ok", async ()=>{
+        await api.get("/status").expect(200)
+    })
+ })
